24554aca1bed2635039eea6131a444b82b4604e1
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2021-10-11 19:49:51 +0700

    Exercise 2

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -2,26 +2,58 @@
 4.	Receive an array of n integers, n is even. 
 Sort the first half in increasing order and second half decreasing order
 Ex:
- ____________________________________
+ __________________________________
 | Input:  8 7 9 2 1 5 2 0            |
 | Output: 2 7 8 9 5 2 1 0            |
 |____________________________________|
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int main(int argc, char *argv[]) {
+void Tang(int A[], int n)
+{
+	for (int i = 0; i < n / 2 - 1; i++)
+		for (int j = i + 1; j < n / 2; j++)
+			if (A[i] > A[j])
+			{
+				int temp;
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+}
+void Giam(int A[], int n)
+{
+	for (int i = n / 2; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (A[i] < A[j])
+			{
+				int temp;
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+}
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
 	int n = argc;
-	int test_array[n],i;
-	for(i=0; i<n;i++){
-		test_array[i] = atoi(argv[i+1]);
+	int test_array[n], i;
+	for (i = 0; i < n; i++)
+	{
+		test_array[i] = atoi(argv[i + 1]);
 	}
 	//Your codes here
-	
-	
+	Giam(test_array, n);
+	for (int i = 0; i < n / 2; i++)
+	{
+		printf(" %d", test_array[i]);
+	}
+	Tang(test_array, n);
+	for (int i = n / 2; i < n; i++)
+	{
+		printf(" %d", test_array[i]);
+	}
 	return 0;
 }
